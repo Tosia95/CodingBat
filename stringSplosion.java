620c14f4074fe20d9a66072cc4cdaf79495647d7
Tosia95 <ola.tusinska@outlook.com>
2016-07-07 14:49:15 +0200

    07.07 15:00

---
+++ stringSplosion.java
@@ -0,0 +1,10 @@
+public String stringSplosion(String str) {
+  
+  String result = "";
+  
+  for (int i = 0; i <= str.length(); i++)
+  {
+    result = result + str.substring(0, i);
+  }
+  return result;
+}
