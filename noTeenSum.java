59d5f7849c6c88626310960ef5c4c574a184cde9
Tosia95 <ola.tusinska@outlook.com>
2016-07-20 14:23:24 +0200

    18.07 14:30

--- noTeenSum.java
+++ noTeenSum.java
@@ -1,32 +1,57 @@
 public int noTeenSum(int a, int b, int c) {
   
+  boolean a1 = fixTeen(a);
+  boolean b1 = fixTeen(b);
+  boolean c1 = fixTeen(c);
   int sum = 0;
   int d = 0;
-  if (fixTeen(a) != 0) {
-    d = a;
-  }
-    sum +=d;
-  if (fixTeen(b) != 0) {
-    d = b;
-  }
-    sum += d;
-  if (fixTeen(c) != 0) {
-    d += c; 
-  }
-    sum += d;
-  return sum;
-}
-public int fixTeen(int n) {
-    
-    int res = 0;
-    if (n < 13 && n > 19 || n == 15 || n == 16) {
-      res = n;
-    }
-    else if (n >= 13 && n <=19) {
-      if (n != 15 && n != 16) {
-        res = 0;
+  if (a1) {
+    if (b1) {
+      if (c1) {
+        sum = a+b+c;
+      }
+      else {
+        sum = a+b;
       }
     }
+    else {
+      if (c1) {
+        sum = a+c;
+      }
+      else {
+        sum = a;
+      }
+    }
+  }
+  else {
+    if (b1) {
+      if (c1) {
+        sum = b+c;
+      }
+      else {
+        sum = b;
+      }
+    }
+    else {
+      if (c1) {
+        sum = c;
+      }
+      else { 
+        sum = 0;
+    }
+  }
+}
+  return sum;
+}
+public boolean fixTeen(int n) {
     
-    return res;
+    if (n == 15 || n == 16) {
+      return true;
+    }
+    else if (n >= 13 && n <= 19) {
+      return false;
+    }
+    else {
+        return true;
+    }
 }
