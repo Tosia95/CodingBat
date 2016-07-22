70a96b0edb776e0e77a42232ad9215fb5e51c23e
Tosia95 <ola.tusinska@outlook.com>
2016-07-22 13:09:57 +0200

    22.07 13:00

--- makeChocolate.java
+++ makeChocolate.java
@@ -1,24 +1,18 @@
 public int makeChocolate(int small, int big, int goal) {
   
   int s = 0;
-  if (goal < 5) {
-    if (small >= goal%5) {
+  
+  if (goal < big * 5 && small >= goal % 5) {
+      s =  goal%5;
+  }
+  else if (goal / 5 < big && small >= goal % 5) { 
       s = goal%5;
     }
-    else {
-      s = -1;
-    }
+  else if (goal / 5 >= big && small >= goal - big * 5) { 
+    s = goal - big * 5;  
   }
-  else if (goal/5 >= big) {
-    s = goal%5;
-  }
-  else {
-    if (goal%5 < small) {
-      s = goal - big + goal%5;
-    }
-    else {
-      s = -1;
-    }
+  else if (big * 5 + small < goal || small < goal % 5) {
+    s = -1;
   }
   return s;
 }
