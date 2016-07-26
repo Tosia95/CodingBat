cbbc7a31fb4045d7fc19bc30369244cf1938937b
Tosia95 <ola.tusinska@outlook.com>
2016-07-26 15:14:33 +0200

    26.07 15:00

---
+++ zipZap.java
@@ -0,0 +1,11 @@
+public String zipZap(String str) {
+  
+
+  if (str.length() < 3) return str;
+  for (int i = 0; i <str.length()-2; i++) {
+    if (str.charAt(i) == 'z' && str.charAt(i+2) == 'p') {
+      str = str.substring(0, i+1) + str.substring(i+2);
+    }
+  }
+  return str;
+}
